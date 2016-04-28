b9512302813de799236f7e00330b66ce0b0d08af
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-04-29 03:19:24 +0900

    predictor

---
+++ predict.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys
+import os.path
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.models import model_from_json
+
+MODEL_FILE = './data/model.json'
+WEIGHT_FILE = './data/weight.hdfs'
+
+#
+# Preparing data
+#
+x = np.array([sys.argv[1].split(',')], dtype=np.float32)
+
+
+#
+# Load model
+#
+model = model_from_json(open(MODEL_FILE).read())
+model.load_weights(WEIGHT_FILE)
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+#
+# Prediction
+#
+print model.predict_classes(x, batch_size=32, verbose=1)
